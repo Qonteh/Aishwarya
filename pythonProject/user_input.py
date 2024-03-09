959b9bd8109a497f649b542cfdb60a8897df0f9f
abdulyusuph051@gmail.com <abdulyusuph051@gmail.com>
2024-03-09 19:15:57 +0300

    invoice Generator

--- pythonProject/user_input.py
+++
@@ -1,9 +0,0 @@
-
-name = input("Enter your name: ")
-
-age = input("Enter your age: ")
-
-
-location = input("Enter your location: ")
-
-print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
